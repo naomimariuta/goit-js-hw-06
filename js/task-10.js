e2ce0b3708ee03013d4de9264cdb4f36c4bfaf7a
naomimariuta <157979968+naomimariuta@users.noreply.github.com>
2024-06-17 23:24:47 +0300

    task 10 commit

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,38 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const inputNumber = document.querySelector("input[type=number]");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+	if (amount >= 1 && amount <= 100) {
+		for (let i = 0; i < amount; i++) {
+			boxes.insertAdjacentHTML(
+				"beforeend",
+				`<div style="width:${Math.floor(30 + 10 * i)}px; 
+        height: ${Math.floor(30 + 10 * i)}px; 
+        background-color:${getRandomHexColor()};"></div>`
+			);
+		}
+	} else {
+		alert("Introduceti un numar intre 1 si 100!");
+	}
+};
+
+const creation = () => {
+	boxes.innerHTML = null;
+	createBoxes(inputNumber.value);
+};
+
+const destroyBoxes = () => {
+	boxes.innerHTML = null;
+	console.log("Distrugere perfecta ^_^");
+};
+
+createButton.addEventListener("click", creation);
+destroyButton.addEventListener("click", destroyBoxes);
